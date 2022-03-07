577243c473c9d01317d356b6be7652dcdd68ff92
KatrinKBR <ka.bethencourt@duocuc.cl>
2022-03-07 18:32:46 -0300

    added first two components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CarruselComponent } from './carrusel/carrusel.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarruselComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule
