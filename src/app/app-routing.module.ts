e7a71194f40fa07f88f33ed6393933344054f9de
KatrinKBR <ka.bethencourt@duocuc.cl>
2022-03-18 15:01:15 -0300

    added routing for landing and not found components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { CarritoComponent } from './components/carrito/carrito.component';
 import { InfoComponent } from './components/info/info.component';
+import { LandingComponent } from './components/landing/landing.component';
 import { ListadoComponent } from './components/listado/listado.component';
 import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 import { RegistroComponent } from './components/registro/registro.component';
 
 const routes: Routes = [
+  { path: '', component: LandingComponent},
   { path: 'registro', component: RegistroComponent },
   { path: 'login', component: LoginComponent },
   { path: 'listado', component: ListadoComponent },
   { path: 'info', component: InfoComponent },
-  { path: 'carrito', component: CarritoComponent }
+  { path: 'carrito', component: CarritoComponent },
+  { path: '**', component: NotFoundComponent}
 ]
 
 @NgModule({
